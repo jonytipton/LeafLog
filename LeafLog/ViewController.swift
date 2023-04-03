04c892daf987ea05a2b17ed85c0accb19bbb858d
Jonathan Tipton <nature_explorer98@outlook.com>
2023-04-03 14:08:20 -0600

    Initial Commit

---
+++ LeafLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeafLog
+//
+//  Created by Jonathan Tipton on 4/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
