4ffe49bd65207df065e7b17b44171de3e55b0d6a
Jonathan Tipton <nature_explorer98@outlook.com>
2023-05-01 15:39:31 -0600

    Updates to DetailSlideshowCell - Updated constraints on ImageScrollView to show previous and next cards - Updated UI to better show horizontal scroll option

--- LeafLog/MyGarden/DetailSlideshowCell.swift
+++ LeafLog/MyGarden/DetailSlideshowCell.swift
@@ -14,7 +14,7 @@ class DetailSlideshowCell: UITableViewCell, UIScrollViewDelegate {
     @IBOutlet var scrollContentView: UIView!
     
     var images: [UIImage] = []
-    var padding: CGFloat = 15
+    var padding: CGFloat = 5
     var cornerRadius: CGFloat = 25
     var currentPage = 0
     
@@ -32,12 +32,14 @@ class DetailSlideshowCell: UITableViewCell, UIScrollViewDelegate {
         for (index,image) in images.enumerated() {
             let view = UIImageView(image: image)
             view.contentMode = .scaleAspectFill
-            view.frame = CGRect(x: imageScrollView.frame.width * CGFloat(index) + padding, y: padding, width: imageScrollView.frame.width - (2 * padding), height: imageScrollView.frame.height - (2 * padding))
+            view.frame = CGRect(x: imageScrollView.frame.width * CGFloat(index) + padding, y: padding, width: imageScrollView.frame.width - (2 * padding), height: imageScrollView.frame.height - (4 * padding))
             view.layer.cornerRadius = cornerRadius
             view.layer.masksToBounds = true
             scrollContentView.addSubview(view)
             scrollContentView.widthAnchor.constraint(equalToConstant: imageScrollView.frame.width * CGFloat(images.count)).isActive = true
             imageScrollView.isPagingEnabled = true
+            imageScrollView.clipsToBounds = false
+            
         }
     }
 }
